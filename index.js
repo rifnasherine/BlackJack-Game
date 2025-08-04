71ec684e295f14474058e3d1fc81d0665aa454d0
rifnasherine <119393279+rifnasherine@users.noreply.github.com>
2025-08-04 23:27:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+
+let cards =[]
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let hasStarted = false
+let message ="";
+let player = {
+    name : "Rifna",
+    coins : 150
+}
+
+let messageEl = document.getElementById("message-el")
+let cardEl = document.getElementById("card-el")
+//let sumEl = document.getElementById("sum-el")
+let sumEl = document.querySelector("#sum-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.coins
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random()*13)+1
+    if(randomNumber==1){
+       return 11
+    }
+    else if(randomNumber>10){
+         return 10
+    }
+    return randomNumber
+}
+function startGame(){
+    hasStarted = true
+    isAlive = true
+    hasBlackJack = false
+    cards=[]
+    sum = 0
+    cards.push(getRandomCard())
+    cards.push(getRandomCard())
+    sum+=cards[0]+cards[1]
+    renderGame()
+}
+function renderGame(){
+    cardEl.textContent = "Cards: "
+    for(let i=0; i<cards.length; i++){
+        cardEl.textContent += cards[i] +" "
+    }
+    sumEl.textContent = "Sum: " + sum
+    if(sum<=20){
+        message = "Do you want to draw another card?"
+    }
+    else if(sum === 21){
+        message = "You have got a blackJack!"
+        hasBlackJack = true;
+    }
+    else{
+        message ="You are out of the game"
+        isAlive = false
+    }
+    messageEl.textContent = message;  
+}
+
+function newCard(){
+    if(isAlive && !hasBlackJack){
+    let newCard = getRandomCard()
+    sum+=newCard
+    cards.push(newCard)
+    renderGame()
+    }
+    else if(hasBlackJack){
+        messageEl.textContent = "You have already won!"
+    }
+    else if(!isAlive && hasStarted){
+        messageEl.textContent = "You cannot draw cards anymore"
+    }
+    else {
+        messageEl.textContent = "Start the game to draw a new card"
+    }
+    
+    
+}
+
